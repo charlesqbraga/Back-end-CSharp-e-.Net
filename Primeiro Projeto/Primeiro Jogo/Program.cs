91c90665aa81f704c073c3115b55b7f576e9e76a
Charles Braga <charlesqbraga.dev@gmail.com>
2023-07-27 03:36:32 -0300

    menu 1  e  2 funcionando

---
+++ Primeiro Projeto/Primeiro Jogo/Program.cs
@@ -0,0 +1,29 @@
+﻿//Jogo do número secreto
+
+
+
+Random aleatorio = new Random(); //Numeros random
+int numeroSecreto = aleatorio.Next(1, 101); //Define o número secreto
+
+do
+{
+    Console.Write("Digite um número entre 1 e 100: ");
+    int chute = int.Parse(Console.ReadLine()); // converte no Parse.
+
+    if (chute == numeroSecreto)
+    {
+        Console.WriteLine("Parabéns! Você acertou o número em cheio.");
+        break;
+    }
+    else if (chute < numeroSecreto)
+    {
+        Console.WriteLine("O número é maior do que o escolhido.");
+    }
+    else
+    {
+        Console.WriteLine("O número é menor do que o escolhido.");
+    }
+
+} while (true);
+
+Console.WriteLine("O jogo acabou. Você acertou o número secreto, Parabens! :)");
--- Primeiro Projeto/Primeiro Projeto/Program.cs
+++ Primeiro Projeto/Primeiro Projeto/Program.cs
@@ -1,7 +1,6 @@
 ﻿// Screen Sound
 string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
-List<string> listaDasBandas = new List<string> { "U2", "The Beatles", "Calypso" };
-
+List<string> listaDasBandas = new List<string>(); //criamos uma lista vazia 
 void ExibirLogo()
 {
     Console.WriteLine(@"
@@ -14,4 +13,81 @@ void ExibirLogo()
 ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
 ");
     Console.WriteLine(mensagemDeBoasVindas);
+
 }
+
+void ExibirOperacoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção:");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcãoEscolhidaNumerica = int.Parse(opcaoEscolhida); //<-- Converção da opção escolhida que era string para int.
+
+    switch (opcãoEscolhidaNumerica)
+    {
+        case 1:
+            RegistrarBanda();
+            break;
+        case 2:
+            MostrarBandasRegistradas();
+            break;
+        case 3:
+            Console.WriteLine("Você escolheu a opção " + opcãoEscolhidaNumerica);
+            break;
+        case 4:
+            Console.WriteLine("Você escolheu a opção " + opcãoEscolhidaNumerica);
+            break;
+        case -1:
+            Console.WriteLine("Volte sempre :) ");
+            break;
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+
+void RegistrarBanda()
+{
+    Console.Clear(); //Comando para Limpar o Console.
+    Console.WriteLine("******************");
+    Console.WriteLine("Registro de Bandas");
+    Console.WriteLine("******************\n");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!; //Sinal de ! no final para mostrar que não queremos trabalhar com valor nulo.
+    listaDasBandas.Add(nomeDaBanda);
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso"); // o simbulo de $ no começo da frase junto das chaves {} se chama interpolação de string
+    Thread.Sleep(2000); // essa função serve para esperar, no caso ele vai esperar 2 milissegundos
+    Console.Clear();
+    ExibirOperacoesDoMenu();
+}
+
+void MostrarBandasRegistradas()
+{
+    Console.Clear(); //Comando para Limpar o Console.
+    Console.WriteLine("************************************");
+    Console.WriteLine("Exibindo todas as bandas registradas");
+    Console.WriteLine("************************************\n"); // o simbulo de \n serve para pular uma linha
+    /*for (int i = 0; i < listaDasBandas.Count; i++)
+        Console.WriteLine($"Banda: {listaDasBandas[i]}");*/
+
+    foreach (string banda in listaDasBandas)
+    {
+        Console.WriteLine($"Banda: {banda}");
+    }
+    Console.WriteLine("\nDigite uma tecla para voltar ao menu principal.");
+    Console.ReadKey();
+    Console.Clear();
+    ExibirOperacoesDoMenu();
+}
+
+
+//Essa parte chama os void's.
+ExibirLogo();
+ExibirOperacoesDoMenu();
