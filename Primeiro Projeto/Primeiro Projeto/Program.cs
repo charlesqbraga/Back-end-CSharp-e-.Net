cb8cad114c465c7d958b900374f1a3f706636616
Charles Braga <charlesqbraga.dev@gmail.com>
2023-07-28 02:25:14 -0300

    Adicionado a opção 3 para avaliar uma banda registrada

--- Primeiro Projeto/Primeiro Projeto/Program.cs
+++ Primeiro Projeto/Primeiro Projeto/Program.cs
@@ -3,7 +3,7 @@ string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
 //List<string> listaDasBandas = new List<string>(); //criamos uma lista vazia 
 Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
 
-bandasRegistradas.Add("Linkin Park", new List<int> { 10, 8, 6});
+bandasRegistradas.Add("Linkin Park", new List<int> { 10, 8, 6 });
 bandasRegistradas.Add("The Beatles", new List<int>());
 void ExibirLogo()
 {
@@ -42,8 +42,8 @@ void ExibirOperacoesDoMenu()
             MostrarBandasRegistradas();
             break;
         case 3:
-            Console.WriteLine("Você escolheu a opção " + opcãoEscolhidaNumerica);
-            break;
+            AvaliarUmaBanda();     
+                break;
         case 4:
             Console.WriteLine("Você escolheu a opção " + opcãoEscolhidaNumerica);
             break;
@@ -70,6 +70,7 @@ void RegistrarBanda()
 
 void MostrarBandasRegistradas()
 {
+    Console.Clear();
     ExibirTituloDaOpcao("Exibindo todas as bandas registradas."); // o simbulo de \n serve para pular uma linha
     /*for (int i = 0; i < listaDasBandas.Count; i++)
         Console.WriteLine($"Banda: {listaDasBandas[i]}");*/
@@ -91,7 +92,37 @@ void ExibirTituloDaOpcao(string titulo)
     Console.WriteLine(asteriscos);
     Console.WriteLine(titulo);
     Console.WriteLine(asteriscos + "\n");
-        
+
+}
+
+void AvaliarUmaBanda()
+{
+    //digite qual banda deseja avaliar
+    //se a banda existir no dicionário >> atribuir uma nota
+    //senão, volta ao menu principal
+    Console.Clear();
+    ExibirTituloDaOpcao("Avaliar banda");
+    Console.Write("Digite o nome da banda que deseja avaliar: ");
+    string nomeDaBanda = Console.ReadLine()!;
+    if (bandasRegistradas.ContainsKey(nomeDaBanda))
+    {
+        Console.Write($"Digite a nota que a banda {nomeDaBanda} merece: ");
+        int nota = int.Parse(Console.ReadLine()!);
+        bandasRegistradas[nomeDaBanda].Add(nota);
+        Console.WriteLine($"\nA nota {nota} foi registrada com sucesso para a banda {nomeDaBanda}. ");
+        Thread.Sleep(4000);
+        Console.Clear();
+        ExibirOperacoesDoMenu();
+    }
+    else
+    {
+        Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
+        Console.WriteLine("Digite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOperacoesDoMenu();
+    }
+
 }
 
 //Essa parte chama os void's.
